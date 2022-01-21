e88fc6685a6ca746424865a887ec3e5730f1846b
Reggie <reggiectout@gmail.com>
2022-01-21 17:01:53 +0100

    need to edit service admin

--- src/Controller/admin/ServicesController.php
+++ src/Controller/admin/ServicesController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/services')]
+#[Route('/services/admin')]
 class ServicesController extends AbstractController
 {
     #[Route('/', name: 'services_index', methods: ['GET'])]
