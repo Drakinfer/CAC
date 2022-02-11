0b0839dd451d1822581b727f7a1f483334d14c99
Reggie <reggiectout@gmail.com>
2022-02-11 09:20:08 +0100

    modification de service

--- src/Controller/ServiceEntrepriseController.php
+++ src/Controller/ServiceEntrepriseController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EntrepriseRepository;
+use App\Repository\ServicesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ServiceEntrepriseController extends AbstractController
 {
     #[Route('/service/entreprise', name: 'service_entreprise')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository, ServicesRepository $servicesRepository): Response
     {
         return $this->render('service_entreprise/index.html.twig', [
-            'controller_name' => 'ServiceEntrepriseController',
+            'services' => $servicesRepository->triEntreprises(),
+            'entreprise' => $entrepriseRepository->find(1),
         ]);
     }
 }
--- src/Controller/ServiceParticulierController.php
+++ src/Controller/ServiceParticulierController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EntrepriseRepository;
+use App\Repository\ServicesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ServiceParticulierController extends AbstractController
 {
     #[Route('/service/particulier', name: 'service_particulier')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository, ServicesRepository $servicesRepository): Response
     {
         return $this->render('service_particulier/index.html.twig', [
-            'controller_name' => 'ServiceParticulierController',
+            'services' => $servicesRepository->triParticuliers(),
+            'entreprise' => $entrepriseRepository->find(1),
         ]);
     }
 }
